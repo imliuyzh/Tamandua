8c9bebadf280e9f5cac9ebf3e8e3999c1a7e6044
Yizhen Liu <imliuyzh@outlook.com>
2019-12-28 16:34:15 -0800

    Add preset.py to help to archive the inputs

---
+++ app/src/preset.py
@@ -0,0 +1,14 @@
+"""This module is for the users who want to avoid to keep typing in the
+same information again and again."""
+
+
+# Specify which quarter do you want to look for. Please follow this format:
+# "2020 SPRING" (including quotation marks)
+YEAR_TERM = None
+
+# Keep a set of classes you want to watch for. Please follow this format:
+# {'35590', '12452', '56231'}
+COURSE_SET = None
+
+# Please enclose the address in quotation marks, as in "tamandua@gmail.com"
+EMAIL_ADDRESS = None
